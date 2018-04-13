23617834c1ea4822c86aa6e9001d981c6102bd57
pkumar <pankaj.kumar33@vodafone.com>
2018-04-13 14:30:48 +0530

    initial commit

---
+++ src/main/java/com/example/netflixoss/springeurekaclientschoolservice/SpringEurekaClientSchoolServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.netflixoss.springeurekaclientschoolservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringEurekaClientSchoolServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEurekaClientSchoolServiceApplication.class, args);
+	}
+}
